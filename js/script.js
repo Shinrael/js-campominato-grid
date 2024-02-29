257590fe8f77d9ab824b2a2f4c97a0da88af5ddc
Shinrael <mircoelena89@gmail.com>
2024-02-29 16:52:52 +0100

    [add] function reset at button

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 // Creo la const del container
 
 const container = document.querySelector('.me-container');
+const btnReset = document.getElementById('btn-reset');
 
-reset();
+btnReset.addEventListener('click', function(){
+  reset();
+})     
 
 for(let i = 1; i <= 100; i++){
   const square = createSquare(i);
@@ -19,7 +22,8 @@ for(let i = 1; i <= 100; i++){
 
 
 function reset() {
-  container.innerHTML = '';
+  const squares = document.querySelectorAll('.square');
+  squares.forEach(square => square.classList.remove('active'));
 }
 
 // Creo la funzione per creare il quadrato
